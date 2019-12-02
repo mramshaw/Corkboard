35a44911ec2022669d310100d4f8c183333392ae
mramshaw <mramshaw@alumni.concordia.ca>
2019-12-02 09:42:45 -0500

    Updates

---
+++ precache-manifest.8bcc6a17e34c4b3c8a4d3e1ae6eb232e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180f165d575f0d8597569d684b143f32",
+    "url": "/Corkboard/index.html"
+  },
+  {
+    "revision": "bd9e46f0f7f4d0aacc8f",
+    "url": "/Corkboard/static/css/main.5e0ebf76.chunk.css"
+  },
+  {
+    "revision": "cf0000a76c1bff568317",
+    "url": "/Corkboard/static/js/2.2c148137.chunk.js"
+  },
+  {
+    "revision": "bd9e46f0f7f4d0aacc8f",
+    "url": "/Corkboard/static/js/main.6ec34ea6.chunk.js"
+  },
+  {
+    "revision": "ce3f6a93a1b93341c388",
+    "url": "/Corkboard/static/js/runtime-main.5416f8b2.js"
+  },
+  {
+    "revision": "e912c8e044b524a309a8844f470e3150",
+    "url": "/Corkboard/static/media/corkboard.e912c8e0.png"
+  }
+]);
